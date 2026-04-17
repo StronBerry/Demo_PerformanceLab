ecf3cf77c796a914f125323352e1506ed8d52890
Malinin S <stronberry@MacBook-Pro-Stron.local>
2026-04-17 13:10:05 +0300

    + ADD_TO_CART_PATTERN var goodsList CartPage upd CartTest upd

--- src/test/java/saucedemo/tests/CartTest.java
+++ src/test/java/saucedemo/tests/CartTest.java
@@ -1,20 +1,39 @@
 package saucedemo.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import saucedemo.base.BaseTest;
 
+import java.util.List;
+
+import static org.testng.Assert.*;
+
 public class CartTest extends BaseTest {
 
+    private final List<String> goodsList = List.of(
+            "Test.allTheThings() T-Shirt (Red)",
+            "Sauce Labs Onesie",
+            "Sauce Labs Fleece Jacket",
+            "Sauce Labs Backpack"
+    );
+
     @Test
-    public void openCartTest() {
-        loginPage.openLoginPage();
+    public void checkGoodsInCart() {
+
+        loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
 
-        Assert.assertTrue(productPage.isPageOpened());
+        for (String goods : goodsList) {
+            productPage.addToCart(goods);
+        }
+        assertEquals(productPage.getCartBadgeText(), String.valueOf(goodsList.size()));
+        productPage.switchToCart();
 
-        productPage.clickCart();
+        assertTrue(cartPage.isPageOpened());
+        assertFalse(cartPage.getProductsNames().isEmpty());
+        assertEquals(cartPage.getProductsNames().size(), goodsList.size());
 
-        Assert.assertTrue(cartPage.isPageOpened());
+        for (String goods : goodsList) {
+            assertTrue(cartPage.getProductsNames().contains(goods));
+        }
     }
 }
